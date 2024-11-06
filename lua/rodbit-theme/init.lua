a9d21dc90f425fb713f7ffc42bdf40657ea83e83
rodbit-studios <rodbit.studios@gmail.com>
2024-11-06 12:37:50 -0500

    test

--- lua/rodbit-theme/init.lua
+++ lua/rodbit-theme/init.lua
@@ -1,85 +1,90 @@
 local M = {}
 
-local colors = require("rodbit-theme.colors")
-
 function M.setup()
-	vim.cmd("highlight clear")
-	if vim.fn.exists("syntax_on") then
-		vim.cmd("syntax reset")
+	local colors = {
+		rodbit_dark = "#272935",
+		rodbit_light = "#fafafa",
+		rodbit_error = "#f87d88",
+		rodbit_success = "#aef8b0",
+		rodbit_warning = "#ffc857",
+		rodbit_info = "#60d5f6",
+		rodbit_purple = "#dd9ff8",
+		rodbit_comment = "#c481ff",
+	}
+
+	local theme = {
+		Normal = { fg = colors.rodbit_light, bg = colors.rodbit_dark },
+		DiffText = { fg = colors.rodbit_error, bg = "NONE" },
+		ErrorMsg = { fg = colors.rodbit_error, bg = "NONE" },
+		WarningMsg = { fg = colors.rodbit_error, bg = "NONE" },
+		PreProc = { fg = colors.rodbit_error, bg = "NONE" },
+		Exception = { fg = colors.rodbit_error, bg = "NONE" },
+		Error = { fg = colors.rodbit_error, bg = "NONE" },
+		DiffDelete = { fg = colors.rodbit_error, bg = "NONE" },
+		GitGutterDelete = { fg = colors.rodbit_error, bg = "NONE" },
+		GitGutterChangeDelete = { fg = colors.rodbit_error, bg = "NONE" },
+		cssIdentifier = { fg = colors.rodbit_error, bg = "NONE" },
+		cssImportant = { fg = colors.rodbit_error, bg = "NONE" },
+		Type = { fg = colors.rodbit_error, bg = "NONE" },
+		Identifier = { fg = colors.rodbit_error, bg = "NONE" },
+		PMenuSel = { fg = colors.rodbit_success, bg = "NONE" },
+		Constant = { fg = colors.rodbit_success, bg = "NONE" },
+		Repeat = { fg = colors.rodbit_success, bg = "NONE" },
+		DiffAdd = { fg = colors.rodbit_success, bg = "NONE" },
+		GitGutterAdd = { fg = colors.rodbit_success, bg = "NONE" },
+		cssIncludeKeyword = { fg = colors.rodbit_success, bg = "NONE" },
+		Keyword = { fg = colors.rodbit_success, bg = "NONE" },
+		IncSearch = { fg = colors.rodbit_warning, bg = "NONE" },
+		Title = { fg = colors.rodbit_warning, bg = "NONE" },
+		PreCondit = { fg = colors.rodbit_warning, bg = "NONE" },
+		Debug = { fg = colors.rodbit_warning, bg = "NONE" },
+		SpecialChar = { fg = colors.rodbit_warning, bg = "NONE" },
+		Conditional = { fg = colors.rodbit_warning, bg = "NONE" },
+		Todo = { fg = colors.rodbit_warning, bg = "NONE" },
+		Special = { fg = colors.rodbit_warning, bg = "NONE" },
+		Label = { fg = colors.rodbit_warning, bg = "NONE" },
+		Delimiter = { fg = colors.rodbit_warning, bg = "NONE" },
+		Number = { fg = colors.rodbit_warning, bg = "NONE" },
+		CursorLineNR = { fg = colors.rodbit_warning, bg = "NONE" },
+		Define = { fg = colors.rodbit_warning, bg = "NONE" },
+		MoreMsg = { fg = colors.rodbit_warning, bg = "NONE" },
+		Tag = { fg = colors.rodbit_warning, bg = "NONE" },
+		String = { fg = colors.rodbit_warning, bg = "NONE" },
+		MatchParen = { fg = colors.rodbit_warning, bg = "NONE" },
+		Macro = { fg = colors.rodbit_warning, bg = "NONE" },
+		DiffChange = { fg = colors.rodbit_warning, bg = "NONE" },
+		GitGutterChange = { fg = colors.rodbit_warning, bg = "NONE" },
+		cssColor = { fg = colors.rodbit_warning, bg = "NONE" },
+		Function = { fg = colors.rodbit_info, bg = "NONE" },
+		Directory = { fg = colors.rodbit_purple, bg = "NONE" },
+		markdownLinkText = { fg = colors.rodbit_purple, bg = "NONE" },
+		javaScriptBoolean = { fg = colors.rodbit_purple, bg = "NONE" },
+		Include = { fg = colors.rodbit_purple, bg = "NONE" },
+		Storage = { fg = colors.rodbit_purple, bg = "NONE" },
+		cssClassName = { fg = colors.rodbit_purple, bg = "NONE" },
+		cssClassNameDot = { fg = colors.rodbit_purple, bg = "NONE" },
+		Statement = { fg = colors.rodbit_light, bg = "NONE" },
+		Operator = { fg = colors.rodbit_light, bg = "NONE" },
+		cssAttr = { fg = colors.rodbit_light, bg = "NONE" },
+		Pmenu = { fg = colors.rodbit_light, bg = "#454545" },
+		SignColumn = { bg = colors.rodbit_dark },
+		LineNr = { fg = "#949494", bg = colors.rodbit_dark },
+		NonText = { fg = colors.rodbit_purple, bg = colors.rodbit_dark },
+		Comment = { fg = colors.rodbit_comment, gui = "italic" },
+		SpecialComment = { fg = colors.rodbit_comment, gui = "italic", bg = "NONE" },
+		CursorLine = { bg = "#454545" },
+		TabLineFill = { gui = "NONE", bg = "#454545" },
+		TabLine = { fg = "#949494", bg = "#454545", gui = "NONE" },
+		StatusLine = { gui = "bold", bg = "#454545", fg = colors.rodbit_light },
+		StatusLineNC = { gui = "NONE", bg = colors.rodbit_dark, fg = colors.rodbit_light },
+		Search = { bg = colors.rodbit_purple, fg = colors.rodbit_light },
+		VertSplit = { gui = "NONE", fg = "#454545", bg = "NONE" },
+		Visual = { gui = "NONE", bg = "#454545" },
+	}
+
+	for group, colors in pairs(theme) do
+		vim.api.nvim_set_hl(0, group, colors)
 	end
-
-	vim.o.termguicolors = true
-	vim.g.colors_name = "rodbit-theme"
-
-	-- Highlight function
-	local function hi(group, color)
-		local style = color.style and "gui=" .. color.style or "gui=NONE"
-		local fg = color.fg and "guifg=" .. color.fg or "guifg=NONE"
-		local bg = color.bg and "guibg=" .. color.bg or "guibg=NONE"
-		vim.cmd(string.format("highlight %s %s %s %s", group, fg, bg, style))
-	end
-
-	-- Editor Highlights
-	hi("Normal", { fg = colors.secondary.light_gray, bg = colors.bg.normal })
-	hi("NormalFloat", { fg = colors.secondary.light_gray, bg = colors.bg.lighter })
-	hi("FloatBorder", { fg = colors.primary.blue })
-	hi("LineNr", { fg = colors.primary.gray, bg = colors.bg.normal })
-	hi("CursorLineNr", { fg = colors.primary.blue, bg = colors.bg.lighter, style = "bold" })
-	hi("CursorLine", { bg = colors.bg.lighter })
-	hi("SignColumn", { bg = colors.bg.normal })
-
-	-- Syntax Highlights
-	hi("Comment", { fg = colors.fg.comment, style = "italic" })
-	hi("Constant", { fg = colors.tertiary.teal })
-	hi("String", { fg = colors.additional.green })
-	hi("Character", { fg = colors.additional.dark_green })
-	hi("Number", { fg = colors.secondary.yellow })
-	hi("Boolean", { fg = colors.primary.red })
-	hi("Float", { fg = colors.secondary.yellow })
-
-	-- [Rest of the highlight definitions]
-
-	-- Popup Menu
-	hi("Pmenu", { fg = colors.secondary.light_gray, bg = colors.bg.lighter })
-	hi("PmenuSel", { fg = colors.primary.blue, bg = colors.bg.statusline, style = "bold" })
-	hi("PmenuSbar", { bg = colors.bg.lighter })
-	hi("PmenuThumb", { bg = colors.primary.blue })
-
-	-- Search and Selection
-	hi("Search", { fg = colors.bg.normal, bg = colors.secondary.yellow })
-	hi("IncSearch", { fg = colors.bg.normal, bg = colors.primary.red })
-	hi("Visual", { bg = colors.secondary.dark_blue })
-
-	-- Diagnostic & Git
-	hi("DiagnosticError", { fg = colors.primary.red })
-	hi("DiagnosticWarn", { fg = colors.secondary.yellow })
-	hi("DiagnosticInfo", { fg = colors.primary.blue })
-	hi("DiagnosticHint", { fg = colors.tertiary.teal })
-
-	hi("GitSignsAdd", { fg = colors.additional.green })
-	hi("GitSignsChange", { fg = colors.primary.blue })
-	hi("GitSignsDelete", { fg = colors.primary.red })
-
-	-- Telescope
-	hi("TelescopeBorder", { fg = colors.primary.blue })
-	hi("TelescopePromptBorder", { fg = colors.primary.blue })
-	hi("TelescopeResultsBorder", { fg = colors.primary.blue })
-	hi("TelescopePreviewBorder", { fg = colors.primary.blue })
-
-	-- Treesitter Highlights
-	hi("@variable", { fg = colors.secondary.light_gray })
-	hi("@property", { fg = colors.tertiary.teal })
-	hi("@function", { fg = colors.primary.blue, style = "bold" })
-	hi("@constructor", { fg = colors.additional.purple })
-	hi("@keyword", { fg = colors.primary.red })
-	hi("@type", { fg = colors.tertiary.teal })
-	hi("@string", { fg = colors.additional.green })
-	hi("@comment", { fg = colors.fg.comment, style = "italic" })
-
-	-- LSP
-	hi("LspReferenceText", { bg = colors.bg.lighter })
-	hi("LspReferenceRead", { bg = colors.bg.lighter })
-	hi("LspReferenceWrite", { bg = colors.secondary.dark_blue })
 end
 
 function M.load()
